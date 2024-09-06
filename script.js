e5eb7b5354041495b2a668a69dbd6a9fccc07a58
mpesaran <mahsa.pesaran@gmail.com>
2024-09-06 10:02:18 +1000

    some extra features added to the grid
    1. randomizing rgbs of square when mouse enters the square
    2.adding opacity to the color of cells when mouse hovers on.
    each time the mouse enters the cell opacity increases by 0.1 until reaches 1.

--- script.js
+++ script.js
@@ -2,15 +2,24 @@ const container = document.getElementById('container');
 
 function makeRows(rowCount) {
     container.innerHTML = "";
-
+    const containerWidth = parseFloat(getComputedStyle(container).width);
     const cellSize = parseFloat((960 / rowCount).toFixed(2));
-    container.style.setProperty('--square-size', `${cellSize}px`);
 
+
+    console.log(containerWidth)
     for (let r = 0; r < rowCount * rowCount; r++) {
         let cell = document.createElement('div');
-        cell.classList.add("cell");
+        cell.style.minWidth = `${cellSize}px`;
+        cell.style.minHeight = `${cellSize}px`;
+        cell.style.opacity = 0;
+
         container.appendChild(cell);
     }
+    const divs = container.querySelectorAll("div");
+
+    divs.forEach((div) => {
+        div.addEventListener("mouseenter", hoverOnDivs);
+    });
 }
 
 function handleButtonClick() {
@@ -20,8 +29,26 @@ function handleButtonClick() {
         alert("Please enter a valid number between 1 and 100")
         return;
     }
-
     makeRows(newSize)
+}
+
+function random(min, max) {
+    const num = Math.floor(Math.random() * (max - min)) + min;
+    return num
+}
+
+function setRandomColor() {
+    return `rgb(${random(0, 255)} , ${random(0, 255)} , ${random(0, 255)})`;
+}
+
+
+function hoverOnDivs(event) {
+    let div = event.target;
+    div.style.backgroundColor = setRandomColor();
+
+    let currentOpacity = parseFloat(div.style.opacity);
+    currentOpacity = Math.min(currentOpacity + 0.1, 1);
+    div.style.opacity = currentOpacity;
 }
 
 document.getElementById('generateGrid').addEventListener('click', handleButtonClick);
